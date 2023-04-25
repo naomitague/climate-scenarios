3f0e39325bf909703cbff70e4a608cdc1df63d48
Mallory Giesie <mallorygiesie@icloud.com>
2023-04-25 23:37:56 +0000

    adding seperate R files for each function in build_runs workflow

---
+++ build_runs.R
@@ -0,0 +1,77 @@
+#' Build Runs Function
+#'
+#' This function builds runs of filtered data frames based on provided parameters.
+#'
+#' @param rcp Integer specifying the Representative Concentration Pathway (RCP) scenario.
+#' @param gcm Character specifying the General Circulation Model (GCM).
+#' @param sample_cell Integer specifying the sample cell.
+#' @param our_gcm Optional data frame to use as input. If provided, other arguments (rcp, gcm, sample_cell) will be ignored.
+#' @param run_type Character specifying the type of runs to build. Either "year" or "season".
+#' @param start_date Character specifying the start date for building the runs in format "MM/DD/YYYY".
+#' @param duration Integer specifying the duration of runs in years.
+#' @param climate_table List of data frames containing climate variable information for filtering.
+#' @param sample_window Function to generate sample window of data.
+#'
+#' @return A list of filtered data frames, one for each climate variable in the climate_table, ready to be used in the stitches function.
+#'
+#' @examples
+#' build_runs(
+#'   rcp = 45,
+#'   gcm = "MIROC5",
+#'   sample_cell = 2,
+#'   start_date = '04/07/2023',
+#'   sample_window = 10,
+#'   duration = 10,
+#'   climate_table = climate_vars_list,
+#'   run_type = "season"
+#' )
+build_runs <- function(rcp = NULL,
+                       gcm = NULL,
+                       sample_cell = NULL,
+                       our_gcm = NULL,
+                       run_type,
+                       start_date,
+                       duration,
+                       climate_table,
+                       sample_window) {
+  
+  if (!is.null(our_gcm)) {
+    # If our_gcm argument is provided, use it as the input data frame
+    our_gcm <- our_gcm %>% mutate(season = ifelse(lubridate::month(time) %in%  c(11, 12, 1, 2, 3, 4), "wet", "dry"))
+      
+  } else if (!is.null(rcp) & !is.null(gcm) & !is.null(sample_cell)) {
+    # If rcp, gcm, and sample_cell arguments are provided, find the data frame using find_df() function
+    our_gcm <- find_df(rcp, gcm, sample_cell) %>% add_season()
+  } else {
+    stop("Either provide rcp, gcm, and sample_cell arguments or provide a dataframe using our_gcm argument.")
+  }
+  
+  df_list <- list()
+  
+  season_list <- season_order(start_date, length(climate_table))
+  
+  for (i in 1:length(climate_vars_list)) {
+    start_date <- as.Date(start_date, format = "%m/%d/%Y", origin = "1970-01-01")
+    
+    df <- sample_window(start_date, duration, our_gcm)
+    
+    if(run_type == "year") {
+      runs <- filter_df(df, climate_table[[i]])
+    }
+    
+    if(run_type == "season") {
+      # Call filter_df at index i of the climate variables list
+      runs <- filter_df(df, climate_table[[i]], season_list[[i]])
+    }
+    
+    # Appends the output to the df_list
+    df_list[[i]] <- runs
+    print(df_list)
+    start_date <- start_date + lubridate::years(1) # Add one year to start_date
+  }
+  
+  # Returns the list of filtered years for each data frame that can be put into the stitches function
+  return(df_list)
+}
+
+
---
+++ filter_df.R
@@ -0,0 +1,63 @@
+#' Filter Data Frame Based on Climate Table and Season List
+#'
+#' This function filters a data frame based on a climate table and an optional season list.
+#'
+#' @param df Data frame to filter.
+#' @param climate_table Climate table, which is a data frame specifying the climate variable, lower percentile,
+#' upper percentile, and corresponding thresholds for filtering.
+#' @param season_list Optional season list to filter the data frame by season. Default is NULL.
+#' @return Filtered data frame or a concatenated string of water years and seasons.
+#' @examples
+#' filter_df(df, climate_table, season_list = c("winter", "spring"))
+#' filter_df(df, climate_table)
+#' @export
+filter_df <- function(df, climate_table, season_list = NULL) {
+  
+  if (!is.null(season_list)) {
+    # Group data frame by water year and season, and calculate summary statistics
+    df <- df %>% group_by(water_year, season) %>% summarise(wind = mean(wind),
+                                                            max_temp = mean(max_temp),
+                                                            min_temp = mean(min_temp),
+                                                            precip = sum(precip),
+                                                            min_humidity = mean(min_humidity),
+                                                            max_humidity = mean(max_humidity))
+    
+    for (j in 1:nrow(climate_table)) {
+      df <- df %>% filter(season %in% season_list) # Filter by season list
+      
+      climate_var <- climate_table[j, "variable"]
+      lower <- climate_table[j, "lower"] 
+      upper <- climate_table[j, "upper"]
+      
+      lower_perc <- quantile(df[[climate_var]], lower, na.rm = TRUE) # Calculate lower percentile
+      
+      upper_perc <- quantile(df[[climate_var]], upper, na.rm = TRUE) # Calculate upper percentile
+      
+      df <- df %>% filter(!is.na(!!sym(climate_var)), !!sym(climate_var) >= lower_perc, !!sym(climate_var) <= upper_perc) # Filter by thresholds
+    }
+    
+    return(paste(df$water_year, df$season)) # Return concatenated string of water years and seasons
+  } else {
+    # Group data frame by water year and calculate summary statistics
+    df <- df %>% group_by(wy = water_year) %>% summarise(wind = mean(wind),
+                                                         max_temp = mean(max_temp),
+                                                         min_temp = mean(min_temp),
+                                                         precip = sum(precip),
+                                                         min_humidity = mean(min_humidity),
+                                                         max_humidity = mean(max_humidity))
+    
+    for (j in 1:nrow(climate_table)) {
+      climate_var <- climate_table[j, "variable"]
+      lower <- climate_table[j, "lower"] 
+      upper <- climate_table[j, "upper"]
+      
+      lower_perc <- quantile(df[[climate_var]], lower, na.rm = TRUE) # Calculate lower percentile
+      
+      upper_perc <- quantile(df[[climate_var]], upper, na.rm = TRUE) # Calculate upper percentile
+      
+      df <- df %>% filter(!is.na(!!sym(climate_var)), !!sym(climate_var) >= lower_perc, !!sym(climate_var) <= upper_perc) # Filter by thresholds
+    }
+    
+    return(df$wy) # Return water years
+  }
+}
